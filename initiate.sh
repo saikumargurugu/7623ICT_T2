3a3471571be894bdba466145706b1282abde5442
sai kumar G <saikumar.gurugu@gmail.com>
2023-09-26 18:22:22 +1000

    clean up initi

--- initiate.sh
+++ initiate.sh
@@ -4,18 +4,15 @@ mv ToDoList-v2 todo_list
 
 rm -r ToDoList-v2
 
-# docker pull mongo
-# docker pull ngnix
-
 openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout RootCA.key -out RootCA.pem -subj "/C=US/CN=My-Root-CA"
 openssl x509 -outform pem -in RootCA.pem -out RootCA.crt
-openssl req -new -nodes -newkey rsa:2048 -keyout nodecrud.key -out nodecrud.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=nodecrud.local"
-openssl x509 -req -sha256 -days 1024 -in nodecrud.csr -CA RootCA.pem -CAkey RootCA.key -CAcreateserial -extfile domains.ext -out nodecrud.crt
+openssl req -new -nodes -newkey rsa:2048 -keyout todoapp.key -out todoapp.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=todoapp.local"
+openssl x509 -req -sha256 -days 1024 -in todoapp.csr -CA RootCA.pem -CAkey RootCA.key -CAcreateserial -extfile domains.ext -out todoapp.crt
 mkdir ssl
-mv nodecrud.* ssl/
+mv todoapp.* ssl/
 mv RootCA.* ssl/
 
-docker build -t crud .
+docker build -t todoapp .
 
 
 # db.createUser({user:'admin',
